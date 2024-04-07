281c7ce370c583ba04544adfe911ffdb5e6020d0
Yujiro Yukawa <yuj.yuk@icloud.com>
2024-04-07 18:59:19 +0900

    課題提出

--- src/practice/Chapter10.java
+++ src/practice/Chapter10.java
@@ -1,10 +1,33 @@
 package practice;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Chapter10 {
+    public static void main(String[] args) throws Exception {
+        List<Task> list = new ArrayList<>();
+        list.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う", true));
+        list.add(new Task(LocalDate.of(2021, 9, 15), "○○社面接", false));
+        list.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う", true));
+        list.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く", false));
+        list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+        // 未完了のタスクの個数を表示
+        long incompleteCount = list.stream()
+                .filter(task -> !task.isDone())
+                .count();
+        System.out.println("未完了のタスクの個数は" + incompleteCount);
 
-	}
+        // 未完了のタスク一覧を日時の昇順に並べて表示
+        Collections.sort(list);
 
+        System.out.println("【未完了のタスクを昇順に並び替えて一覧表示】");
+        for (Task task : list) {
+            if (!task.isDone()) {
+                System.out.println(task.getDate() + " " + task.getTask());
+            }
+        }
+    }
 }
--- src/practice/Task.java
+++ src/practice/Task.java
@@ -1,5 +1,41 @@
+
 package practice;
 
-public class Task {
+import java.time.LocalDate;
 
+public class Task implements Comparable<Task> {
+    private LocalDate date;
+    private String task;
+    private boolean done; // 追加　完了はtrue, 未完了は false
+
+	public Task(LocalDate date, String task, boolean done) {
+		this.date = date;
+		this.task = task;
+		this.done = done;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public String getTask() {
+		return task;
+	}
+
+	// boolean型のgetterメソッドはis～とすることもあります
+	public boolean isDone() {
+		return done;
+	}
+
+	// compareToメソッドをオーバーライドする
+	// 日付で並び替えるため、LocalDateのcompareToを使う
+	@Override
+	public int compareTo(Task t) {
+		return this.date.compareTo(t.date);
+	}
+
+	@Override
+	public String toString() {
+		return this.date + " " + this.task;
+	}
 }
